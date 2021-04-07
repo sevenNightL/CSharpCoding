944b8514028b0da62465d35ebb8bcd9c02ce79f1
xuhao <xhlin52@gmail.com>
2021-04-07 21:42:38 +0800

    添加项目文件。

---
+++ ObserverPattern/FishType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObserverPattern
+{
+    public enum FishType
+    {
+        Crucian,
+        Carp,
+        SnakeHead,
+        BlackCarp,
+        GrassCarp,
+        Weever
+    }
+}
