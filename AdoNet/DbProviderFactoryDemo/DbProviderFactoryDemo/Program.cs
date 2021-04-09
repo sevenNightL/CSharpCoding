b971b0449824c31081b29007ee180e288dea440c
xuhao <xhlin52@gmail.com>
2021-04-09 18:09:32 +0800

    add dbfactory file

---
+++ AdoNet/DbProviderFactoryDemo/DbProviderFactoryDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Configuration;
+using System.Data.Common;
+
+namespace DbProviderFactoryDemo
+{
+    class Program
+    {
+        public static DbProviderFactory dataFactory =
+DbProviderFactories.GetFactory(
+ConfigurationManager.AppSettings["provider"]);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
